517d4234ea7a61f48f2ac4a9156e4bb18459e354
anoopprasad <anoop.prasad5@gmail.com>
2020-07-15 14:41:26 +0530

    Customize SCIM rails to support v2

--- app/controllers/scim_rails/application_controller.rb
+++ app/controllers/scim_rails/application_controller.rb
@@ -4,35 +4,12 @@ module ScimRails
     include ExceptionHandler
     include Response
 
-    before_action :authorize_request
+    before_action :find_site
 
     private
 
-    def authorize_request
-      send(authentication_strategy) do |searchable_attribute, authentication_attribute|
-        authorization = AuthorizeApiRequest.new(
-          searchable_attribute: searchable_attribute,
-          authentication_attribute: authentication_attribute
-        )
-        @company = authorization.company
-      end
-      raise ScimRails::ExceptionHandler::InvalidCredentials if @company.blank?
-    end
-
-    def authentication_strategy
-      if request.headers["Authorization"]&.include?("Bearer")
-        :authenticate_with_oauth_bearer
-      else
-        :authenticate_with_http_basic
-      end
-    end
-
-    def authenticate_with_oauth_bearer
-      authentication_attribute = request.headers["Authorization"].split(" ").last
-      payload = ScimRails::Encoder.decode(authentication_attribute).with_indifferent_access
-      searchable_attribute = payload[ScimRails.config.basic_auth_model_searchable_attribute]
-
-      yield searchable_attribute, authentication_attribute
+    def find_site
+      @site = Site.first
     end
   end
 end
--- app/controllers/scim_rails/scim_users_controller.rb
+++ app/controllers/scim_rails/scim_users_controller.rb
@@ -4,7 +4,7 @@ module ScimRails
       if params[:filter].present?
         query = ScimRails::ScimQueryParser.new(params[:filter])
 
-        users = @company
+        users = @site
           .public_send(ScimRails.config.scim_users_scope)
           .where(
             "#{ScimRails.config.scim_users_model.connection.quote_column_name(query.attribute)} #{query.operator} ?",
@@ -12,7 +12,7 @@ module ScimRails
           )
           .order(ScimRails.config.scim_users_list_order)
       else
-        users = @company
+        users = @site
           .public_send(ScimRails.config.scim_users_scope)
           .order(ScimRails.config.scim_users_list_order)
       end
@@ -28,28 +28,26 @@ module ScimRails
 
     def create
       if ScimRails.config.scim_user_prevent_update_on_create
-        user = @company.public_send(ScimRails.config.scim_users_scope).create!(permitted_user_params)
+        user = @site.public_send(ScimRails.config.scim_users_scope).create!(permitted_user_params)
       else
         username_key = ScimRails.config.queryable_user_attributes[:userName]
         find_by_username = Hash.new
         find_by_username[username_key] = permitted_user_params[username_key]
-        user = @company
+        user = @site
           .public_send(ScimRails.config.scim_users_scope)
           .find_or_create_by(find_by_username)
         user.update!(permitted_user_params)
       end
-      update_status(user) unless put_active_param.nil?
       json_scim_response(object: user, status: :created)
     end
 
     def show
-      user = @company.public_send(ScimRails.config.scim_users_scope).find(params[:id])
+      user = @site.public_send(ScimRails.config.scim_users_scope).find(params[:id])
       json_scim_response(object: user)
     end
 
     def put_update
-      user = @company.public_send(ScimRails.config.scim_users_scope).find(params[:id])
-      update_status(user) unless put_active_param.nil?
+      user = @site.public_send(ScimRails.config.scim_users_scope).find(params[:id])
       user.update!(permitted_user_params)
       json_scim_response(object: user)
     end
@@ -57,8 +55,7 @@ module ScimRails
     # TODO: PATCH will only deprovision or reprovision users.
     # This will work just fine for Okta but is not SCIM compliant.
     def patch_update
-      user = @company.public_send(ScimRails.config.scim_users_scope).find(params[:id])
-      update_status(user)
+      user = @site.public_send(ScimRails.config.scim_users_scope).find(params[:id])
       json_scim_response(object: user)
     end
 
