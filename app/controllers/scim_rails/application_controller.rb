39c8452246e93e36ca6ba4c4d32abbad2f96afe8
Prateek Mittal <prateek@bangthetable.com>
2020-07-15 23:38:17 +0530

    Adding a way to validate jwt signature and setting current site

--- app/controllers/scim_rails/application_controller.rb
+++ app/controllers/scim_rails/application_controller.rb
@@ -4,12 +4,25 @@ module ScimRails
     include Response
 
     before_action :doorkeeper_authorize!
+    before_action :validate_jwt_token!
     before_action :set_current_site
 
     private
 
     def set_current_site
-      @site ||= Site.first
+      @site ||= Site.find(doorkeeper_token.aud)
+    end
+
+    def validate_jwt_token!
+      ::JWT.decode(
+        doorkeeper_token.token,
+        OpenSSL::PKey::RSA.new(File.read(Doorkeeper::JWT.configuration.secret_key_path)),
+        true,
+        { algorithm: Doorkeeper::JWT.configuration.encryption_method.to_s.upcase }
+      )
+    rescue DecodeError
+      # TODO: Revoke the token
+      head :unauthorized
     end
   end
 end
